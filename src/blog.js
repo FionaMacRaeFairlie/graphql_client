57d9f2c60f5c57c9a60710c5d89d66dea44cf0f2
Fiona Fairlie <f.fairlie@gcu.ac.uk>
2024-12-02 13:26:45 +0000

    update className typo

--- src/blog.js
+++ src/blog.js
@@ -2,9 +2,9 @@ export const Blog = ({ content, author, id }) => {
   return (
     
     <div key={id} className="card" style={{ width: "75rem" }}>
-      <div class="card-header">A post from {author}</div>
+      <div className="card-header">A post from {author}</div>
       <div className="card-body">
-        <p class="card-text">{content}</p>
+        <p className="card-text">{content}</p>
       </div>
     </div>
   );
